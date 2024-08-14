5f9c5e894f6de33c23863071d04e980ceacd24e5
JaywordNerd <jarthur@acsint.io>
2024-08-14 01:21:14 +0000

    first push

---
+++ Mathics/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Mathics
+{
+    class Program
+    {
+        static int Adder(int x)
+        {
+            return x + 5;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            string x = Console.ReadLine();
+            int y = int.Parse(x);
+            Console.WriteLine(Adder(y));
+        }
+
+        
+    }
+}
